c56475d8d83c2f6ea1b348cce9b8a6ebfbe8810d
Rafael93150 <tavaresrafael93@gmail.com>
2021-12-10 18:25:14 +0100

    console log test

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const io = require('socket.io')(http);
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
+console.log('test');
+
 // app.use(express.json());
 
 // app.use(express.static('client/build'));
