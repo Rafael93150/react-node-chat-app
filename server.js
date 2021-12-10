df4f58e74bfc692a929966ca92f40fbea1acfd95
Rafael93150 <tavaresrafael93@gmail.com>
2021-12-10 18:32:32 +0100

    console log test

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 require('dotenv').config({path: './config/.env'}); //Variables d'environnement
-// require('./config/db.js');
+
 
 const express = require('express');
-// const userRoutes = require('./routes/user.routes');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
@@ -12,20 +11,6 @@ const PORT = process.env.PORT || 5000;
 
 console.log('test');
 
-// app.use(express.json());
-
-// app.use(express.static('client/build'));
-
-// app.get('/*', (_, res) => {
-//   res.sendFile(path.join(__dirname, './client/build/index.html'))
-// });
-
-// app.get('/', (_, res) => {
-//     var path = require('path');
-//     res.sendFile(path.resolve(__dirname + '/../public/index.html'));
-// })
-
-// app.use('/api/user', userRoutes);
 let connectCounter;
 
 io.on('connection', (socket) => {
