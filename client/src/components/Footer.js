2382c3c3ba8b029178d7d3274fef795319bd65a9
Rafael93150 <tavaresrafael93@gmail.com>
2021-12-13 19:43:37 +0100

    Home

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className="footer">
-            <p>Développé par <a href="https://rafaeltavares.fr">Rafael Tavares</a></p>
+            <p>Developed by <a href="https://rafaeltavares.fr">Rafael Tavares</a></p>
         </div>
     )
 };
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
 
     return (
         <div className="home">
+            <h1>Free Online Chat With No Registration</h1>
+            <p>
+                This web site is an online free chat. <br/>
+                With him, you can meet new friends from all over the world. For that, u dont need to registrate or download the application. <br/>
+            </p>
+            <div className="home-mobile">
+                <h3>Mobile</h3>
+                <p>This web site also supports mobile, so you can chat from where u want. </p>
+            </div>
+                He was developped to learn React and NodeJS basics.
+            
+            <NavLink className="button" to="/chat">
+                Start Chat Now
+            </NavLink>
         </div>
+        
     );
 };
 
